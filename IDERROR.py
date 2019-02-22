7850b3a11af230e4bd8e0760fb2cbebb4f8b174c
yas19sin <yas19sin@gmail.com>
2019-02-22 05:16:54 +0000

    first commit

---
+++ IDERROR.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.Qsci import *
+
+form_ui = 'design/Editor.ui'
+
+
+class Editor(QMainWindow):
+    def __init__(self, parent=None):
+        super(Editor, self).__init__(parent)
+        self.ui = loadUi(form_ui)
+
+    def Setup(self):
+        self.ui.setFixedSize(800, 800)
+        self.editor = QsciScintilla()
+        self.editor.setText("Hello\n")
+        self.editor.append("world")
+        self.editor.setLexer(None)
+        self.editor.setUtf8(True)  # Set encoding to UTF-8
+        self.Font = QFont()
+        self.Font.setPointSize(14)
+        self.editor.setFont(self.Font)  # Will be overridden by lexer!
+        self.ui.addWidget(self.editor)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    QApplication.setStyle(QStyleFactory.create('Fusion'))
+    myGUI = Editor()
+    myGUI.Setup()
+    myGUI.show()
+
+    sys.exit(app.exec_())
